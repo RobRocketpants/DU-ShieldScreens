3c2afab080a0f9860722a69ed68ea3e2a94f0d8e
RobRocketpants <94449488+RobRocketpants@users.noreply.github.com>
2021-11-23 02:07:01 +0000

    Added parameter driven customization
    Parameter driven for now so as not to need a databank yet

--- src/shield_pb.lua
+++ src/shield_pb.lua
@@ -22,6 +22,74 @@ end
 
 --unit.start()
 
+RGBcolor1 = "201,164,61" --export: Shield Bar, Highlight and Update color
+RGBcolor2 = "135,206,235" --export: Outlines, Text and Controls color
+RGBbackground = "0,26,26" --export: Background color
+
+FOX_Logo = true --export: 
+
+function validateComp(n,dc)
+    local c = n
+    if type(n)~="number" then
+        c=dc
+    else
+        if n>255 then c=255 end
+        if n<0 then c=0 end
+    end    
+    return c
+end
+
+function validateRGB(t,d)
+    local o = {}
+    if #t~=3 then
+        o = d
+    else
+        o[1] = validateComp(t[1],d[1])
+        o[2] = validateComp(t[2],d[2])
+        o[3] = validateComp(t[3],d[3])
+    end
+    return o
+end
+
+function colorSplit(col)
+    local colt = {}
+    for match in (col..","):gmatch("(.-),") do
+        table.insert(colt, tonumber(match));
+    end   
+    return colt
+end
+
+color1 = colorSplit(RGBcolor1)
+color2 = colorSplit(RGBcolor2)
+bg = colorSplit(RGBbackground)
+
+color1 = validateRGB(color1,{201,164,61})
+color2 = validateRGB(color2,{135,206,235})
+bg = validateRGB(bg,{0,26,26})
+
+local FOXlogo = [[
+    local fontf = getFont('Montserrat-Bold',15)
+    setDefaultFillColor(layer, Shape_Polygon, color.r, color.g, color.b, 1)
+    setDefaultFillColor(layer, Shape_Box, color.r, color.g, color.b, 1)
+    setDefaultStrokeColor(layer, Shape_Box, color.r, color.g, color.b, 1)
+    setDefaultStrokeWidth(layer, Shape_Box, 0)
+    setDefaultStrokeColor(layer, Shape_Polygon, color.r, color.g, color.b, 1)
+    setDefaultStrokeWidth(layer, Shape_Polygon, 0)
+    addBox(layer, rx/2 -3, ry -45, 6, 6)
+    addQuad(layer, rx/2 +3, ry -45, rx/2 +9, ry -51, rx/2 +15, ry -51, rx/2 +3, ry -39)
+    addQuad(layer, rx/2 -3, ry -45, rx/2 -9, ry -51, rx/2 -15, ry -51, rx/2 -3, ry -39)
+    addBox(layer, rx/2 -15, ry -69, 6, 18)
+    addBox(layer, rx/2 +9, ry -69, 6, 18)
+    addTriangle(layer, rx/2 -15, ry -69, rx/2 -15, ry -81, rx/2 -3, ry -69)
+    addTriangle(layer, rx/2 +15, ry -69, rx/2 +15, ry -81, rx/2 +3, ry -69)
+    setNextTextAlign(layer, AlignH_Center, AlignV_Bottom)
+    setDefaultFillColor(layer, Shape_Text, color.r, color.g, color.b, 1)
+    addText(layer, fontf, 'FOX Technology', rx/2, ry - 15)
+]]
+
+local screenLogo = [[]]
+
+if FOX_Logo then screenLogo = FOXlogo end
 
 unit.hide()
 
